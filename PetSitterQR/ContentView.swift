5a72e36daae28cdeeffcafc06f51bd262f8538e6
Sebastien <sebastienlato@gmail.com>
2025-11-16 20:56:03 -0500

    Initial Commit

---
+++ PetSitterQR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetSitterQR
+//
+//  Created by sebastien lato on 2025-11-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetSitterQR/PetSitterQRApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetSitterQRApp.swift
+//  PetSitterQR
+//
+//  Created by sebastien lato on 2025-11-16.
+//
+
+import SwiftUI
+
+@main
+struct PetSitterQRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
